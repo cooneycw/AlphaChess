fb8af27b8e2390a9f58d51c7cb89552646047eed
cooneycw <cooneycw@icloud.com>
2023-03-19 12:00:00 -0400

    Corrected weights / parameters.  Commence training / evaluation.

--- main.py
+++ main.py
@@ -6,11 +6,14 @@ import datetime
 import numpy as np
 import tensorflow as tf
 import multiprocessing as mp
-from config.config import Config
+from config.config import Config, interpolate
 from src_code.agent.agent import AlphaZeroChess, board_to_input, create_network
 from src_code.agent.utils import draw_board, visualize_tree, get_board_piece_count, generate_game_id, save_training_data, load_training_data, scan_redis_for_training_data
 
-
+USE_RAY = True
+if USE_RAY:
+    NUM_WORKERS = mp.cpu_count() - 2
+    ray.init(num_cpus=NUM_WORKERS, num_gpus=0, ignore_reinit_error=True, logging_level=logging.DEBUG)
 
 logging.getLogger('tensorflow').setLevel(logging.WARNING)
 physical_devices = tf.config.list_physical_devices('GPU')
@@ -25,8 +28,9 @@ else:
 def play_games(pass_dict):
     game_id = pass_dict['game_id']
     key_prefix = pass_dict['key_prefix']
+    num_iterations = pass_dict['num_iterations']
     # Initialize the config and agent
-    config = Config(verbosity=False)
+    config = Config(num_iterations, verbosity=False)
 
     # Play the game
     for i in range(config.self_play_games):
@@ -115,15 +119,18 @@ def play_games(pass_dict):
 
 
 def train_model(key_prefix):
-    config = Config(verbosity=False)
+    config = Config(num_iterations=None, verbosity=False)
     agent = AlphaZeroChess(config)
 
     key_list = scan_redis_for_training_data(agent, key_prefix)
     cwc = 0
 
 
-#@ray.remote
-def main(type):
+@ray.remote
+def main(in_params):
+    print(f'in_params: {in_params}')
+    type = in_params['type']
+    num_iterations = in_params['num_iterations']
     print(f'Running the main function with type: {type}')
     key_prefix = 'azChess_trainingData_test'
 
@@ -132,6 +139,7 @@ def main(type):
         pass_dict = dict()
         pass_dict['game_id'] = game_id
         pass_dict['key_prefix'] = key_prefix
+        pass_dict['num_iterations'] = num_iterations
         play_games(pass_dict)
 
     elif type == 'train':
@@ -140,31 +148,48 @@ def main(type):
     return f'Finished running the main function with type: {type} Game ID: {game_id}'
 
 
+def initialize(in_config):
+    network_white = create_network(in_config)
+    network_black = create_network(in_config)
+    outer_agent = AlphaZeroChess(in_config, network_white=network_white, network_black=network_black)
+    outer_agent.save_networks('network_current')
+
+
 if __name__ == '__main__':
     type_list = ['initialize', 'create_training_data', 'train']
     type_id = 1
-    use_ray = False
-    outer_config = Config(verbosity=False)
-    if not use_ray:
-        if type_list[type_id] == 'initialize':
-            print(f'Initializing the networks')
-            network_white = create_network(outer_config)
-            network_black = create_network(outer_config)
-            outer_agent = AlphaZeroChess(outer_config, network_white=network_white, network_black=network_black)
-            outer_agent.save_networks('network_current')
-        else:
-            outcome = main(type=type_list[type_id])
-            print(f'Outcome: {outcome}')
-    elif use_ray:
-        NUM_WORKERS = mp.cpu_count() - 2
-        ray.init(num_cpus=NUM_WORKERS, num_gpus=0, ignore_reinit_error=True, logging_level=logging.DEBUG)
-        outer_config = Config(verbosity=False)
+
+    min_iterations = 100
+    outer_config = Config(num_iterations=min_iterations, verbosity=False)
+
+    if type_list[type_id] == 'initialize':
+        initialize(outer_config)
+
+    if type_list[type_id] != 'initialize' and USE_RAY is False:
+        params = dict()
+        params['type'] = type_list[type_id]
+        params['num_iterations'] = min_iterations
+        outcome = main(params)
+        print(f'Outcome: {outcome}')
+    elif type_list[type_id] != 'initialize' and USE_RAY is True:
+
+        max_num_iterations = 1600
+
+        outer_config = Config(min_iterations, verbosity=False)
 
         start_ind = 0
         while start_ind < outer_config.self_play_games:
             inds = list(range(start_ind, min(start_ind + NUM_WORKERS, outer_config.self_play_games)))
-            params = [type_list[type_id] for _ in range(len(inds))]
-            results = [main.remote(params) for _ in range(len(inds))]
+            params_list = []
+
+            for ind in inds:
+                # Get the type ID for the current index
+                params_item = dict()
+                params_item['type'] = type_list[type_id]
+                params_item['num_iterations'] = int(0.5 + interpolate(min_iterations, max_num_iterations, (min(ind, 5000)/5000)))
+                params_list.append(params_item)
+
+            results = [main.remote(params_list[j]) for j in range(len(inds))]
 
             # Wait for all tasks to complete and get the results
             output = ray.get(results)
@@ -174,4 +199,3 @@ if __name__ == '__main__':
                 print(f'Task {i} output: {result}')
 
             start_ind += NUM_WORKERS
-
