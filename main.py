d1698e52360cf642920e6affba98a6abbd9ecfaa
cooneycw <cooneycw@icloud.com>
2023-04-02 13:30:13 -0400

    Profile removal.

--- main.py
+++ main.py
@@ -20,10 +20,10 @@ import os
 os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
 
 
-USE_RAY = False
+USE_RAY = True
 if USE_RAY:
-    NUM_WORKERS = 12
-    ray.init(num_cpus=NUM_WORKERS, num_gpus=0, ignore_reinit_error=True, logging_level=logging.INFO)
+    NUM_WORKERS = 8
+    ray.init(num_cpus=NUM_WORKERS, num_gpus=0, ignore_reinit_error=True, logging_level=logging.DEBUG)
 
 logging.getLogger('tensorflow').setLevel(logging.WARNING)
 physical_devices = tf.config.list_physical_devices('GPU')
@@ -46,7 +46,7 @@ def main(in_params):
     num_iterations = in_params['num_iterations']
     num_evals = in_params['num_evals']
     print(f'Running the main function with type: {type}')
-    key_prefix = 'azChess_Corsair_prod'
+    key_prefix = 'azChess_Aurora_prod'
 
     if type == 'create_training_data':
         game_id = generate_game_id()
