85e3fa16f9ad81973c98a405f5df7ea98850b5b4
cooneycw <cooneycw@icloud.com>
2023-04-08 14:29:01 -0400

    Remove line profiler.

--- src_code/agent/train.py
+++ src_code/agent/train.py
@@ -79,8 +79,7 @@ def train_model(key_prefix, num_train_records=2000):
             break
 
     for keys in list(key_del_list):
-        # redis_conn.delete(keys)
-        pass
+        redis_conn.delete(keys)
 
     print(f'{len(list(key_del_list))} keys deleted from redis...')
     now = datetime.datetime.now()
