800841eec502b7dd468dcb1f35fe390a608c03af
cooneycw <cooneycw@icloud.com>
2023-06-04 14:53:02 -0400

    Create master and worker ray controller.

--- main_ray_head.py
+++ main_ray_head.py
@@ -24,7 +24,8 @@ NUM_WORKERS = 10
 NUM_GPUS = 1
 
 if USE_RAY:
-    ray.init(address=None, num_cpus=NUM_WORKERS, num_gpus=NUM_GPUS, logging_level=logging.INFO)
+    ray.init(num_cpus=NUM_WORKERS, num_gpus=NUM_GPUS, logging_level=logging.INFO)
+
 
 tf.get_logger().setLevel('ERROR')
 physical_devices = tf.config.list_physical_devices('GPU')
@@ -44,10 +45,10 @@ if __name__ == '__main__':
     @ray.remote
     def worker_function():
         # Your worker logic here
-        while ray.get(ray.worker.global_worker.is_connected.remote()):
-            message = "Hello from worker!"
-            ip_address = socket.gethostbyname(socket.gethostname())
-            return message, ip_address
+        ray_worker_id = ray.worker.global_worker.worker_id.hex()
+        message = f"Hello from worker: {ray_worker_id}"
+
+        return message
 
 
     # Create a list to store the remote workers
@@ -63,7 +64,7 @@ if __name__ == '__main__':
 
     # Print the message and IP address from each worker
     for result in results:
-        message, ip_address = result
-        print(f"Message: {message}\tIP Address: {ip_address}")
+        message = result
+        print(f"Message: {message}")
 
     ray.shutdown()
--- main_ray_worker.py
+++ main_ray_worker.py
@@ -1,5 +1,8 @@
 import ray
+import os
 
+
+os.environ["RAY_WORKER_ID"] = "120 Corsair 20tf"
 # Connect to the Ray cluster
 ray.init(address='192.168.5.132:6379')  # Connects to the Ray head node automatically
 
