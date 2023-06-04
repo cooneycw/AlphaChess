42468931a5e4e8e43cbdd7eb8fb60c76a9b81624
cooneycw <cooneycw@icloud.com>
2023-06-04 15:49:05 -0400

    Create master and worker ray controller.

--- main_ray_worker.py
+++ main_ray_worker.py
@@ -1,16 +1,12 @@
 import ray
-import os
+import time
 from src_code.utils.utils import get_non_local_ip
 
 if __name__ == '__main__':
-    worker = get_non_local_ip()
-    if worker == '192.168.5.133':
-        NUM_CPUS = 12
-        NUM_GPUS = 0
     # Connect to the Ray cluster
-    ray.init(address='192.168.5.132:6379', num_cpus=NUM_CPUS, num_gpus=NUM_GPUS)  # Connects to the Ray head node automatically
+    ray.init(address='192.168.5.132:6379')  # Connects to the Ray head node automatically
 
     # Wait for tasks to be assigned and executed by the worker
-    ray.worker.global_worker.run()
+    time.sleep(10000)
 
 
