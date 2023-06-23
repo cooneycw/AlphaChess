55d5ac10f0e681640f9989af0cd6d2ac94229d31
cooneycw <cooneycw@icloud.com>
2023-06-23 16:27:51 -0400

    No prior value readout.

--- src_code/agent/self_play.py
+++ src_code/agent/self_play.py
@@ -58,17 +58,17 @@ def play_games(pass_dict):
         #
 
         # Collect all prior values from nodes
-        prior_values = [node.prior_value for node in agent.tree.root.all_nodes]
-
-        # Count the occurrences of each prior value
-        prior_value_counts = Counter(prior_values)
-
-        # Get the top 10 most common values and their counts
-        top_10_values = prior_value_counts.most_common(10)
-
-        # Display the top 10 values and counts
-        for value, count in top_10_values:
-            print(f"Value: {value}, Count: {count}")
+        # prior_values = [node.prior_value for node in agent.tree.root.all_nodes]
+        #
+        # # Count the occurrences of each prior value
+        # prior_value_counts = Counter(prior_values)
+        #
+        # # Get the top 10 most common values and their counts
+        # top_10_values = prior_value_counts.most_common(10)
+        #
+        # # Display the top 10 values and counts
+        # for value, count in top_10_values:
+        #     print(f"Value: {value}, Count: {count}")
 
         agent.board.push_uci(uci_move)
 
