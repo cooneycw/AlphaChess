c2fcf30964280b30ca9f6f1032916d5d9a85bd9f
cooneycw <cooneycw@icloud.com>
2023-06-24 21:25:50 -0400

    Testing complete.  Run_type restored to 'ray'.

--- main_ray_head.py
+++ main_ray_head.py
@@ -127,7 +127,7 @@ if __name__ == '__main__':
             train_params['network_name_out'] = network_name_out
             train_params['learning_rate'] = learning_rate
             train_params['opt_type'] = opt_type
-            train_params['run_type'] = 'not_ray'
+            train_params['run_type'] = 'ray'
             train_id = main_ray_gpu.remote(train_params)
 
             result = ray.get(train_id)
@@ -157,7 +157,7 @@ if __name__ == '__main__':
                     params_item['learning_rate'] = learning_rate
                     params_item['network_name'] = network_name_out
                     params_item['game_id'] = pre_eval_ind
-                    params_item['run_type'] = 'not_ray'
+                    params_item['run_type'] = 'ray'
 
                     print(f'Starting game {pre_eval_ind} of {outer_config.train_play_games - 1}')
                     result_id = main_ray_no_gpu.remote(params_item)
