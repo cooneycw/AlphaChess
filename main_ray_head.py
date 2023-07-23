a212d4f68892c84304bf17ed40234cbca8d42f67
cooneycw <cooneycw@icloud.com>
2023-07-23 19:18:09 -0400

    Adamax with learning rate reduced.

--- main_ray_head.py
+++ main_ray_head.py
@@ -52,8 +52,8 @@ if __name__ == '__main__':
     verbosity = False
 
     outer_config = Config(verbosity=verbosity)
-    learning_rate = 0.0001
-    opt_type = 'nadam'
+    learning_rate = 0.00001
+    opt_type = 'adamax'
     # play seed games
     outer_agent = AlphaZeroChess(outer_config, network=None)
     if outer_config.reset_redis is True:
