531bd0dccf4fb3b26439ce16c818aac8b5c1b67c
cooneycw <cooneycw@icloud.com>
2023-07-31 13:42:22 -0400

    Initial network train cycle added + initial probs = 0.

--- src_code/agent/agent.py
+++ src_code/agent/agent.py
@@ -516,6 +516,7 @@ class MCTSTree:
             v = v[0][0]
         else:
             v = 0
+            pi = pi * 0
 
         # Add Dirichlet noise to the prior probabilities
         if leaf_node.name == 'root':
